c3368b54a081dd473e474ce05ca44729b988594b
LoboDeCastro <guilobcast@gmail.com>
2021-10-08 21:29:30 +0200

    more calculations

--- src/ArithmetischeOperationen.java
+++ src/ArithmetischeOperationen.java
@@ -1,8 +1,8 @@
 public class ArithmetischeOperationen {
 
     public static void main(String[] args) {
-        int a = 4;
-        int b = 3;
+        int a = 3;
+        int b = 2;
 
         System.out.println(a + b);
         System.out.println(a - b);
@@ -13,7 +13,32 @@ public class ArithmetischeOperationen {
         //falsch, nur als Beispiel
         int x = 2_0000_0000;
         int y = 2_0000_0000;
-
         System.out.println(x + y);
+
+        //eine der Variablen multiplizieren; kleine Trick
+        double f = (a * 1.0) / b;
+        System.out.println("double: " + f);
+
+        int c = 8;
+        int d = c++;
+        System.out.println("d = " + d);
+        System.out.println("c = " + c);
+
+        int e = ++c;
+        System.out.println("e = " + e);
+
+        printResult(2+3);
+        int A = 2 + 4;
+        printResult(A);
+        int result = add(2,5);
+        printResult(result);
+        printResult(add(7,9));
+    }
+
+    public static void printResult(int result) {
+        System.out.println(result);
+    }
+    public static int add(int x, int y) {
+        return x + y;
     }
 }
--- src/Calculator.java
+++ src/Calculator.java
@@ -7,10 +7,30 @@ public class Calculator {
         char d = 'D';
         String e = "Hello";
 
-        System.out.println(a);
-        System.out.println(b);
-        System.out.println(c);
-        System.out.println(d);
-        System.out.println(e);
+        int x = 4;
+        int y = 9;
+
+        System.out.println(sum(x,y));
+        System.out.println(sub(x,y));
+        System.out.println(mult(x,y));
+        System.out.println(div(x,y));
+        System.out.println(mod(x,y));
+
+
+    }
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+    public static int sub(int a, int b) {
+        return a - b;
+    }
+    public static int mult(int a, int b) {
+        return a * b;
+    }
+    public static int div(int a, int b) {
+        return a / b;
+    }
+    public static int mod(int a, int b) {
+        return a % b;
     }
 }
