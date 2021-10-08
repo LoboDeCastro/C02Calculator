9d7965ce43fe039f80b4f6608900d7a6f6313a92
LoboDeCastro <guilobcast@gmail.com>
2021-10-08 19:58:50 +0200

    variablen deklariert

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,6 +1,16 @@
 public class Calculator {
 
     public static void main(String[] args) {
+        int a = 1;
+        double b = 2.34;
+        boolean c = true;
+        char d = 'D';
+        String e = "Hello";
 
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
     }
 }
