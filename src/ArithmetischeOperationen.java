71e826287f7bdde4cec11cf912f2f0e4db005dbf
LoboDeCastro <guilobcast@gmail.com>
2021-10-08 20:23:12 +0200

    calculations

---
+++ src/ArithmetischeOperationen.java
@@ -0,0 +1,19 @@
+public class ArithmetischeOperationen {
+
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 3;
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+
+        //falsch, nur als Beispiel
+        int x = 2_0000_0000;
+        int y = 2_0000_0000;
+
+        System.out.println(x + y);
+    }
+}
